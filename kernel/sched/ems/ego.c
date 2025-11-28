936a50dce6e6d23a23c66882bbff1881abce43a2
snapdragon teaks <inutilidades639@gmail.com>
2025-11-28 19:26:39 -0300

    sched : ems : fixes

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -686,127 +686,6 @@ skip_find_next_freq:
 	return freq;
 }
 
-/*
- * This function computes an effective utilization for the given CPU, to be
- * used for frequency selection given the linear relation: f = u * f_max.
- *
- * The scheduler tracks the following metrics:
- *
- *   cpu_util_{cfs,rt,dl,irq}()
- *   cpu_bw_dl()
- *
- * Where the cfs,rt and dl util numbers are tracked with the same metric and
- * synchronized windows and are thus directly comparable.
- *
- * The cfs,rt,dl utilization are the running times measured with rq->clock_task
- * which excludes things like IRQ and steal-time. These latter are then accrued
- * in the irq utilization.
- *
- * The DL bandwidth number otoh is not a measured metric but a value computed
- * based on the task model parameters and gives the minimal utilization
- * required to meet deadlines.
- */
-unsigned long ego_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
-				 struct task_struct *p)
-{
-	unsigned long dl_util, util, irq;
-	struct rq *rq = cpu_rq(cpu);
-
-	/*
-	 * Early check to see if IRQ/steal time saturates the CPU, can be
-	 * because of inaccuracies in how we track these -- see
-	 * update_irq_load_avg().
-	 */
-
-	irq = cpu_util_irq(rq);
-	if (unlikely(irq >= max)) {
-		util = irq;
-		goto out;
-	}
-
-	/*
-	 * Because the time spend on RT/DL tasks is visible as 'lost' time to
-	 * CFS tasks and we use the same metric to track the effective
-	 * utilization (PELT windows are synchronized) we can directly add them
-	 * to obtain the CPU's actual utilization.
-	 *
-	 * CFS and RT utilization can be boosted or capped, depending on
-	 * utilization clamp constraints requested by currently RUNNABLE
-	 * tasks.
-	 * When there are no CFS RUNNABLE tasks, clamps are released and
-	 * frequency will be gracefully reduced with the utilization decay.
-	 */
-	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL)
-		util = uclamp_rq_util_with(rq, util, p);
-	dl_util = cpu_util_dl(rq);
-
-	/*
-	 * For frequency selection we do not make cpu_util_dl() a permanent part
-	 * of this sum because we want to use cpu_bw_dl() later on, but we need
-	 * to check if the CFS+RT+DL sum is saturated (ie. no idle time) such
-	 * that we select f_max when there is no idle time.
-	 *
-	 * NOTE: numerical errors or stop class might cause us to not quite hit
-	 * saturation when we should -- something for later.
-	 */
-	if (util + dl_util >= max) {
-		util = util + dl_util;
-		goto out;
-	}
-
-	/*
-	 * OTOH, for energy computation we need the estimated running time, so
-	 * include util_dl and ignore dl_bw.
-	 */
-	if (type == ENERGY_UTIL)
-		util += dl_util;
-
-	/*
-	 * There is still idle time; further improve the number by using the
-	 * irq metric. Because IRQ/steal time is hidden from the task clock we
-	 * need to scale the task numbers:
-	 *
-	 *              max - irq
-	 *   U' = irq + --------- * U
-	 *                 max
-	 */
-	util = scale_irq_capacity(util, irq, max);
-	util += irq;
-
-	/*
-	 * Bandwidth required by DEADLINE must always be granted while, for
-	 * FAIR and RT, we use blocked utilization of IDLE CPUs as a mechanism
-	 * to gracefully reduce the frequency when no tasks show up for longer
-	 * periods of time.
-	 *
-	 * Ideally we would like to set bw_dl as min/guaranteed freq and util +
-	 * bw_dl as requested freq. However, cpufreq is not yet ready for such
-	 * an interface. So, we only do the latter for now.
-	 */
-	if (type == FREQUENCY_UTIL)
-		util += cpu_bw_dl(rq);
-
-out:
-	trace_ego_sched_util(cpu, util, util_cfs, cpu_util_rt(rq),
-		cpu_util_dl(rq), cpu_bw_dl(rq), cpu_util_irq(rq));
-
-	return min(max, util);
-}
-
-static unsigned long ego_get_util(struct ego_cpu *egc)
-{
-	struct rq *rq = cpu_rq(egc->cpu);
-	unsigned long util = ml_cpu_util(egc->cpu);
-	unsigned long max = arch_scale_cpu_capacity(egc->cpu);
-
-	egc->max = max;
-	egc->bw_dl = cpu_bw_dl(rq);
-
-	return ego_cpu_util(egc->cpu, util, max, FREQUENCY_UTIL, NULL);
-}
-
 /**
  * ego_iowait_reset() - Reset the IO boost status of a CPU.
  * @egc: the ego data for the CPU to boost
@@ -961,7 +840,6 @@ static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
 		unsigned long cpu_util, cpu_io_util, cpu_max;
 		unsigned long cpu_boosted_util;
 
-		egc->util = cpu_util = ego_get_util(egc);
 		cpu_boosted_util = freqboost_cpu_boost(cpu, cpu_util);
 		cpu_boosted_util = max(cpu_boosted_util, heavytask_cpu_boost(cpu, cpu_util));
 		cpu_boosted_util = get_boost_pelt_util(capacity_cpu(cpu),
